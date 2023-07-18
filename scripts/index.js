d8274d0d4b790fd939b1ca69153544aebb71b1c7
nicolascossi <77031613+nicolascossi@users.noreply.github.com>
2023-07-18 00:10:23 -0300

    Se imprimen los pedidos del JSON, falta que se haga el calculo de el total

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,65 @@
-/* ABRIR Y CERRAR SIDE NAVBAR */
+/* ADD EVENT LISTENER */
 
-const body = document.querySelector('body'),
-sidebar = body.querySelector('nav'),
-toggle = body.querySelector(".toggle"),
-searchBtn = body.querySelector(".search-box"),
-modeSwitch = body.querySelector(".toggle-switch"),
-modeText = body.querySelector(".mode-text");
-toggle.addEventListener("click" , () =>{
-sidebar.classList.toggle("close");
-})
-searchBtn.addEventListener("click" , () =>{
-sidebar.classList.remove("close");
-})
+document.addEventListener('DOMContentLoaded', consultarAPI());
+
+
+/*  FUNCION TRAER PEDIDOS DE LA API */
+
+function consultarAPI() {
+    const url = 'http://localhost:4000/invoices?_expand=client'
+
+    fetch(url)
+    .then (respuesta => respuesta.json())
+    .then (resultado => mostrarPedidos(resultado))
+    .catch (error => console.log(error))
+}
+
+/* FUNCION MOSTRAR PEDIDOS EN EL HTML */
+
+function mostrarPedidos(invoices) {
+    const contenido = document.querySelector('#pedidos');
+  
+    invoices.forEach(invoice => {
+      const div = document.createElement('div');
+      div.classList.add("invoice");
+  
+      const orderNumber = document.createElement('p');
+      orderNumber.classList.add("order-number");
+      orderNumber.textContent = invoice.id;
+  
+      const clientName = document.createElement('p');
+      clientName.classList.add("client-name");
+      clientName.textContent = invoice.client.name;
+  
+      const invoiceAddress = document.createElement('p');
+      invoiceAddress.classList.add("invoice-address");
+      invoiceAddress.textContent = invoice.delivery_adress;
+  
+      const invoiceTotal = document.createElement('p');
+      invoiceTotal.classList.add("invoice-total");
+
+      const totalAmount =
+
+      invoiceTotal.textContent = "Total: $" + totalAmount;
+
+      console.log(totalAmount)
+  
+      const statusButton = document.createElement('button');
+      statusButton.classList.add("status-button");
+      statusButton.textContent = "Pendiente";
+  
+      div.appendChild(orderNumber);
+      div.appendChild(clientName);
+      div.appendChild(invoiceAddress);
+      div.appendChild(invoiceTotal);
+      div.appendChild(statusButton);
+      contenido.appendChild(div);
+    });
+  }
+  
+  
+
+
+
+
+
