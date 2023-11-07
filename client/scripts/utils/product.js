4868b42d9e32cec24d84377b6d0ebc734e6bc9ad
Nicolas <nicolasmartincossi@gmail.com>
2023-11-07 13:33:51 -0300

    ###

--- client/scripts/utils/product.js
+++ client/scripts/utils/product.js
@@ -1,6 +1,6 @@
-export const calcPeriods = (date, retirementDate, period, manualPeriod) => {
+export const calcPeriods = (date, retirementDate, period) => {
   const difference = (retirementDate ? new Date(retirementDate) : new Date()).getTime() - date.getTime();
   const days = Math.floor(difference / 1000 / 60 / 60 / 24);
   const periodos = days / period;
-  return Math.floor(periodos)
+  return Math.ceil(periodos)
 }
